4132663c1de397a5cb1efbf3cbeda590eb74956d
Imrul Hasan <imrul001@gmail.com>
2017-11-10 19:40:15 +0600

    Add an Enum for ResponseCode..
    Signed-off-by: Imrul Hasan <imrul001@gmail.com>

--- src/main/java/dr/hasan/response/ResponseCode.java
+++ src/main/java/dr/hasan/response/ResponseCode.java
@@ -1,4 +1,25 @@
 package dr.hasan.response;
 
-public class ResponseCode {
+public enum ResponseCode {
+    OPERATION_SUCCESSFUL(100),
+    RECORD_NOT_FOUND(101),
+    AUTHENTICATION_FAILED(1102),
+    INVALID_ARGUMENT(103),
+    INVALID_TRANSACTION_PASSWORD(104),
+    SECURITY_ERROR(106),
+    DATABASE_ERROR(110),
+    RUNTIME_ERROR(500),
+    REMOTE_ERROR(502),
+    INTERNAL_CONNECTION_ERROR(503),
+    INTERNAL_REMOTE_ERROR(1501);
+
+    private int code;
+
+    private ResponseCode(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
 }
